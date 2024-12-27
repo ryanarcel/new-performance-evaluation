261a1f75987b3bf41cc1809fcde0ad2f073e39c5
Ryan Arcel Galendez <ryanargee2@gmail.com>
2024-12-27 14:24:09 +0800

    setup formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,21 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution")
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-    root: true,
-    extends: [
-        "plugin:vue/vue3-essential",
-        "eslint:recommended",
-        "@vue/eslint-config-typescript",
-        "@vue/eslint-config-prettier"
-    ],
-    parserOptions: {
-        ecmaVersion: "latest"
-    },
-    rules: {
-        "vue/multi-word-component-names": "off",
-        "no-undef": "off"
-    }
-}
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'no-undef': 'off',
+    trailingComma: 'on',
+    singleQuote: 'on',
+  },
+};
